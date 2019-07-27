c91c862cbfb9b7d21a98170e1bce56600129b281
Abdurrahman Omar Elkousy <ab.omar@RMGS.CO>
2019-07-27 13:28:08 +0200

    changed name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Vapulus
+# angular-contacts-list
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.8.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'vapulus'`, () => {
+  it(`should have as title 'angular-contacts-list'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('vapulus');
+    expect(app.title).toEqual('angular-contacts-list');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to vapulus!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-contacts-list!');
   });
 });
