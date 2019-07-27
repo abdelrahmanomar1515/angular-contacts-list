ed995b2a4495c65bfe91dac528d38a0bd8257566
Abdurrahman Omar Elkousy <ab.omar@RMGS.CO>
2019-07-27 12:46:10 +0200

    form ui fixes

--- src/app/contacts/add-contact/add-contact.component.ts
+++ src/app/contacts/add-contact/add-contact.component.ts
@@ -1,23 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contact',
   templateUrl: './add-contact.component.html',
-  styleUrls: ['./add-contact.component.scss']
+  styleUrls: ['./add-contact.component.scss'],
 })
 export class AddContactComponent {
   contactForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(fb: FormBuilder) {
     this.contactForm = fb.group({
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
-      countryCode: ['', [Validators.required]],
+      countryCode: [44, [Validators.required]],
       phoneNumber: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
     });
   }
 
+  submitForm() {
+    console.log(this.contactForm)
+  }
 
 }
